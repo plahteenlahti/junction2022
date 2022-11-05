61a872786adb5a89bb9837cad3ccd72f66a47e67
Matias Arola <arola@veristable.com>
2022-11-05 19:43:32 +0200

    chore: remove dotenv

--- finally-backend/functions/src/index.ts
+++ finally-backend/functions/src/index.ts
@@ -3,6 +3,13 @@ import * as cors from 'cors'
 import * as express from 'express'
 import * as functions from 'firebase-functions'
 
+import { defineSecret } from 'firebase-functions/params'
+import { mockPreviewDelivery } from './wolt/mock'
+import { WoltSecrets } from './wolt/types'
+
+const woltApiToken = defineSecret('WOLT_API_TOKEN')
+const woltMerchantId = defineSecret('WOLT_MERCHANT_ID')
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -50,10 +57,26 @@ app.post('/b', (req, res) => {
   // res.send(Widgets.create())
 })
 
-app.get('/helloworld', (req, res) => res.send('Hello World!'))
+app.get('/helloworld', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/mock-wolt', async (req, res) => {
+  const secrets: WoltSecrets = {
+    apiToken: woltApiToken.value(),
+    merchantId: woltMerchantId.value(),
+  }
+
+  await mockPreviewDelivery(secrets)
+
+  res.send('done!')
+})
 // app.put('/:id', (req, res) => res.send(Widgets.update(req.params.id, req.body)));
 // app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
 // app.get('/', (req, res) => res.send(Widgets.list()));
 
 // Expose Express API as a single Cloud Function:
-export const orders = functions.region('europe-west3').https.onRequest(app)
+export const orders = functions
+  .runWith({ secrets: [woltMerchantId, woltApiToken] })
+  .region('europe-west3')
+  .https.onRequest(app)
--- finally-backend/functions/src/wolt/mock.ts
+++ finally-backend/functions/src/wolt/mock.ts
@@ -1,34 +1,15 @@
-import { createDeliveryOrder, getDeliveryFee } from './api'
-import { Wolt } from './types'
+import { previewDelivery } from '.'
+import { WoltSecrets } from './types'
 
-export const MOCK_CONTACT: Wolt.ContactDetails = {
-  name: 'John Doe',
-  phone_number: '+358123456789',
-  send_tracking_link_sms: false,
-}
-export const MOCK_CUSTOMER_SUPPORT: Wolt.CustomerSupport = {
-  email: 'john@doe.com',
-  phone_number: '+358123456789',
-  url: 'www.johndoe.com',
-}
+const TO_ADDRESS = 'Otakaari 24, 02150 Espoo'
+const FROM_ADDRESS = 'Veturitallinkuja, 00520 Helsinki'
 
-export const TO_ADDRESS = 'Otakaari 24, 02150 Espoo'
-export const FROM_ADDRESS = 'Veturitallinkuja, 00520 Helsinki'
-export const SCHEDULED_TIME = new Date(
-  Date.now() + 62 * 60 * 1000
-).toISOString()
-
-export const mockGetDeliveryFee = async () => {
-  return getDeliveryFee(FROM_ADDRESS, TO_ADDRESS, SCHEDULED_TIME)
-}
-export const mockGetDeliveryOrder = async () => {
-  return createDeliveryOrder(
+export const mockPreviewDelivery = async (secrets: WoltSecrets) => {
+  const response = await previewDelivery(
+    secrets,
     FROM_ADDRESS,
     TO_ADDRESS,
-    MOCK_CONTACT,
-    MOCK_CONTACT,
-    MOCK_CUSTOMER_SUPPORT,
-    [],
-    SCHEDULED_TIME
+    new Date(Date.now() + 62 * 60 * 1000)
   )
+  console.log(response)
 }
