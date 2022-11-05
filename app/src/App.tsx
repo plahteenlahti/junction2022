f60cc188fbaa826b66a77f1844da73cc19311242
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2022-11-05 14:10:19 +0200

    feat(lint): ship my lint

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,13 +12,14 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
